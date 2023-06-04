c6cebdd1e0baf863a54c0ad9ce13cc0358905422
Sylwia Kinga Bajak <srychtar@artemis.wszib.edu.pl>
2023-06-04 16:17:11 +0200

    emergency

--- trojkaty.java
+++ trojkaty.java
@@ -4,9 +4,9 @@
 
 class Trojkaty {
     /**
-     * Główna fnukcja programu.
+     * Główna funkcja programu.
      * @param {float} a - Długość pierwszego boku.
-     * @param {float} b - Długość drugiegio boku.
+     * @param {float} b - Długość drugiego boku.
      * @param {float} c - Długość trzeciego boku.
      */
     public static void jakiTrojkat(float a, float b, float c){
@@ -18,11 +18,14 @@ class Trojkaty {
     /** Wyświetla ekran pomocy */
     public static void pomoc(){
         System.out.println("Acme INC. (C) 2022");
-        System.out.println("Program do rozpoznawania rodzaju trójkąra");
+        System.out.println("Program do rozpoznawania rodzaju trójkąta");
         System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
     }
     /** Glowna funkcja */
     public static void main(String... args) {
+
+
+
         if (args.length != 3) {
             pomoc();
             System.exit(1);
@@ -30,7 +33,17 @@ class Trojkaty {
         float a = Float.valueOf(args[0]);
         float b = Float.valueOf(args[1]);
         float c = Float.valueOf(args[2]);
+
+	 if (a < 0 || b < 0 || c < 0) {
+                    System.out.println("Długości boków trójkąta muszą być nieujemne!");
+                    System.exit(2);
+                }
+
         
         jakiTrojkat(a, b, c);
     }
+
+
+
+
 }
